d90aa1636a76d3e4742a28c191f31bb04242ede6
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-27 05:53:18 -0400

    feat: eliminate enviroment folder and use signals in header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  title = signal('my first angular app');
+}
