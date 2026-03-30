2f9983cb6a94a39bf41cad68562ad9a324e65b66
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-30 06:02:02 -0400

    feat: using grettings at home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Grettings } from '../components/grettings/grettings';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
+  imports: [Grettings],
 })
 export class Home {}
